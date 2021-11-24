35b54f7ff46e5743f71006b987054f9973ae7143
Alexandr <alex19best78@gmail.com>
2021-11-24 20:16:50 +0200

    1.0.1-beta

--- src/components/Keyboard/Key.jsx
+++ src/components/Keyboard/Key.jsx
@@ -4,7 +4,7 @@ const defaultClass = 'keyboard-btn';
 
 function Key({children, type, active, disabled, reserved, onClick = () => {}}) {
     return(
-        <button className={`${defaultClass} ${type} ${active ? 'keyboard-btn--active' : ''} ${reserved ? 'keyboard-btn--reserved' : ''} ${disabled ? 'keyboard-btn--disabled' : ''}`} onClick={onClick}>{children}</button>
+        <button disabled={reserved} className={`${defaultClass} ${type} ${active ? 'keyboard-btn--active' : ''} ${reserved ? 'keyboard-btn--reserved' : ''} ${disabled ? 'keyboard-btn--disabled' : ''}`} onClick={onClick}>{children}</button>
     );
 }
 
